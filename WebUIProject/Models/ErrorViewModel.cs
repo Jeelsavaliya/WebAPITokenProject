0116e1fb5c3c742315a205d4e89b2bd6daaa5d1c
Jeelsavaliya <jeelsavaliya007@gmail.com>
2024-12-04 11:03:21 +0530

    Complete Authentication Using Class Library

--- WebUIProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebUIProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
